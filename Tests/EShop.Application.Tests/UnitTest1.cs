6a005e2e995048d93399065a1c416689d00a0209
WiktorStaniszewski <wiktorst.8@gmail.com>
2025-05-30 21:04:30 +0200

    Addition of tests

---
+++ Tests/EShop.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EShop.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/EShop.Service.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EShop.Service.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
